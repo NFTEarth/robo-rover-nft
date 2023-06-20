1c1c6edf252ecdbd71bb818627a3a379e6f65a09
Weston Nelson <dogooddowell@protonmail.com>
2023-06-20 17:37:24 -0500

    Update parameters.ts
    Signed-off-by: Weston Nelson <dogooddowell@protonmail.com>

--- src/consts/parameters.ts
+++ src/consts/parameters.ts
@@ -2,15 +2,15 @@
 
 // Your smart contract address (available on the thirdweb dashboard)
 // For existing collections: import your existing contracts on the dashboard: https://thirdweb.com/dashboard
-export const contractConst = "0x436492DBc2E30E56FaC8F2297BD1964833c0687d";
+export const contractConst = "0xe148cb1b6b2B040FEA525856A9E2F6E25115189A";
 
 // The name of the chain your contract is deployed to.
 // Provide a string for standard chains, or import one from @thirdweb-dev/chains package.
 // Learn more: https://portal.thirdweb.com/react/react.thirdwebprovider#activechain-recommended
-export const chainConst = "mumbai";
+export const chainConst = "arbitrum";
 
 // Configure the primary color for buttons and other UI elements
-export const primaryColorConst = "white";
+export const primaryColorConst = "green";
 
 // Choose between "light" and "dark" mode
 export const themeConst = "dark";
